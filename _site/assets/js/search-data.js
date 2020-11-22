db836547c2dab9e6768e6517eff1f184dad51bd5
Raphael Aggio <raphaelaggio@Raphaels-MBP.hub>
2020-11-22 20:54:00 +1300

    changed base url

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "Episódio 1 - Porque velha e fudida tenha dó",
         "excerpt":"Bem vindos ao nosso primeiro episódio! :) Estamos super felizes de finalmente colocar nosso primeiro episódio no ar. Foi muito trabalho mas valeu cada minuto! Este podcast foi criado com muito amor e carinho, sempre pensando na nossa amada avó e em todos que possam se beneficiar das informações que...","categories": ["Introdução"],
         "tags": ["Biohacking","Introdução"],
-        "url": "https://raphaelaggio.github.io/biohackeandoavovo/introdu%C3%A7%C3%A3o/episodio-1/"
+        "url": "https://raphaelaggio.github.io/introdu%C3%A7%C3%A3o/episodio-1/"
       },{
         "title": "Episódio 2 - Porque vovó esperta faz jejum e se liberta",
         "excerpt":"Devo fazer jejum? Mas o que é jejum na verdade? Por que todo mundo está falando sobre jejum? Bem vindos ao nosso segundo episódio! :) Neste capítulo vamos explicar melhor o que é jejum, seus benefícios, seus riscos e um pouquinho da da sua história. Bora escutar esse episódio e...","categories": ["General"],
         "tags": ["Jejum"],
-        "url": "https://raphaelaggio.github.io/biohackeandoavovo/general/episodio-2/"
+        "url": "https://raphaelaggio.github.io/general/episodio-2/"
       },{
         "title": "Episódio 3 - Porque vovó que faz jejum enterra mais um",
         "excerpt":"Nesse episódio nós falamos dos tipos diferentes de jejum. Como assim? Tem mais de um tipo de jejum? Sim, tem vários. Ouve o episódio pra entender mais sobre jejum e como você pode usar esta informação pra se tornar uma versão melhor de você mesmo. :) ","categories": ["General"],
         "tags": ["Jejum"],
-        "url": "https://raphaelaggio.github.io/biohackeandoavovo/general/episodio-3/"
+        "url": "https://raphaelaggio.github.io/general/episodio-3/"
       }]
